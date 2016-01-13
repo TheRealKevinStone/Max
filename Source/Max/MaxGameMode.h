fcb99faff7336d414992f80f6ffdb98910b48b67
TheRealKevinStone <kevin.stone@rcc.yorkvilleu.ca>
2016-01-12 20:35:39 -0500

    First Commit
    FIRSTIES

---
+++ Source/Max/MaxGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "MaxGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAX_API AMaxGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
