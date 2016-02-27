cc7139e879b3994f253b24351f53d80ce84e7d7e
Christopher Aburto <christopher.aburto@hotmail.com>
2016-02-26 20:11:57 -0500

    interactables
    door created, will implement opening logic/animation next (same for
    chest) pickups after that

---
+++ Source/Max/Interactables/Door.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Max.h"
+#include "Door.h"
+
+
+// Sets default values
+ADoor::ADoor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//creating the collider/object for a door
+	DoorCollider = CreateAbstractDefaultSubobject<UBoxComponent>(TEXT("DoorCollider"));
+
+	//creating a mesh component for the door (model)
+	DoorMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DoorMesh"));
+
+}
+
+// Called when the game starts or when spawned
+void ADoor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADoor::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
