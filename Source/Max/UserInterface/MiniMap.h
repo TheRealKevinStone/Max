0c7b9b98e6030128ec9521f9a0a97a548acbef4e
Christopher Aburto <christopher.aburto@hotmail.com>
2016-07-13 23:33:01 -0400

    djfngavkdfgadf
    pokem

---
+++ Source/Max/UserInterface/MiniMap.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/HUD.h"
+#include "MiniMap.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAX_API AMiniMap : public AHUD
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
